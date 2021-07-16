4550fa2e01e4c1399673171427474c0e30dcbe02
Zeshan Ghafoor <zeshan485@gmail.com>
2021-07-16 12:30:38 +0500

    Article Sorting App: design implemented with button state managment

--- src/articleListing.js
+++ src/articleListing.js
@@ -1,44 +1,66 @@
 import { useState, Fragment } from "react";
 import { connect } from "react-redux";
 import { mostLiked, mostRecent } from "./store/actions/article";
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
 function ArticleListing(props) {
+  const [isDisabledMostLiked, setDisableMostLiked] = useState(true);
+  const [isDisabledMostRecent, setDisableMostRecent] = useState(false);
 
   function sortByUpvotes(){
+    setDisableMostRecent(false);
+    setDisableMostLiked(true);
     props.sortByUpvotes();    
   }
 
   function sortByDate(){
+    setDisableMostRecent(true);
+    setDisableMostLiked(false);
     props.sortByDate();    
   }
 
   return (
-    <div className="App">
-      <button onClick={() => sortByUpvotes()}>Most Upvoted</button>
-      <button onClick={() => sortByDate()}>Most Recent</button>
-      {
-        props.articles.map((article, index) => {
-          return (
-            <Fragment key={index}>
-              <p>
-                {
-                  article.title +" Posted on "+ article.date +" with votes "+ article.upVotes
-                }
-              </p>
-            </Fragment>
-          )
-        })
-      }
-    </div>
+    <Container>
+      <Row>
+        <Col sm={2}><Button variant="primary" disabled={isDisabledMostLiked} onClick={() => sortByUpvotes()}>Most Upvoted</Button></Col>
+        <Col sm={2}><Button variant="primary" disabled={isDisabledMostRecent} onClick={() => sortByDate()}>Most Recent</Button></Col>
+      </Row>
+      
+      
+      <Row xs={1} md={3} className="g-4" style={{margin: "5px"}}>
+        {
+          props.articles.map((article, index) => {
+            return (
+                <Col key={index}>
+                    <Card>
+                      <Card.Body>
+                        <Card.Title>{ article.title }</Card.Title>
+                        <Card.Text>
+                          Up Votes: <b>{ article.upVotes }</b>
+                        </Card.Text>
+                        <Card.Footer>
+                          Date: {article.date}
+                        </Card.Footer>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+            )
+          })
+        }
+      </Row>
+    </Container>
   );
 
 }
 
+
+// Redux store connection
 const mapStateToProps = (state) => {
   return {
     articles: state.articleStore.articles
   };
 };
+
 const mapDispatchToProps = (dispatch) => {
   return {
     sortByUpvotes: (payload = {}) => dispatch(mostLiked(payload)),
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ArticleListing from "./articleListing";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //for react redux
 import { Provider } from 'react-redux'
